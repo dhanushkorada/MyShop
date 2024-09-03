f25dd1e390379df6287247359d3205d80b23237e
Dhanush <dhanushkorada5@gmail.com>
2024-09-03 09:38:25 +0530

    Update routing in App.js

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ const App = () => {
 			<Navbar cartItemCount={cartItems.length} />
 			<div className="container">
 				<Routes>
-					<Route path="/" element={<Home />} />
+					<Route path="/MyShop" element={<Home />} />
 					<Route
 						path="/shop"
 						element={<Shop addToCart={addToCart} />}
